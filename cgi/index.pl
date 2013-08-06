3ef25d67c9ed9b954dbe09f32a6979fd5afa081a
Rene Koch <r.koch@ovido.at>
2013-08-06 16:42:51 +0200

    optimized for fcgid

--- cgi/index.pl
+++ cgi/index.pl
@@ -26,6 +26,7 @@ use strict;
 use warnings;
 
 use CGI qw(param);
+use CGI::Fast;
 use CGI::Carp qw(fatalsToBrowser);
 use CGI::Session;
 
@@ -48,114 +49,78 @@ use BPView::Data;
 use BPView::Web;
 
 
-# global variables
-my $session_cache	= 3600;		# 1 hour
-my $config;
-my $views;
-my $dashboards;
-
-
-# HTML code
-print "Content-type: text/html\n\n" unless defined param;
-
-# CGI sessions
-my $post	= CGI->new;
-my $sid     = $post->cookie("CGISESSID") || undef;
-my $session = new CGI::Session(undef, $sid, {Directory=>File::Spec->tmpdir});
-   $session->expire('config', $session_cache);
-   $session->expire('views', $session_cache);
-   $session->expire('dashboards', $session_cache);
-my $cookie  = $post->cookie(CGISESSID => $session->id);
-#print $post->header( -cookie=>$cookie );
-
-
 # open config files if not cached
 my $conf = BPView::Config->new();
 
-if (! $session->param('config')){
-  
-  # open config file directory and push configs into hash
-  $config = $conf->read_dir( dir => $cfg_path );
-  # validate config
-  exit 1 unless ( $conf->validate( 'config' => $config ) == 0);
-  # cache config
-  $session->param('config', $config);
-  
-}else{
+# open config file directory and push configs into hash
+my $config = $conf->read_dir( dir => $cfg_path );
+# validate config
+exit 1 unless ( $conf->validate( 'config' => $config ) == 0);
 
-  # use cached config
-  $config = $session->param('config');
+my $views = $conf->read_dir( dir	=> $cfg_path . "/views" );
+# replaces possible arrays in views with hashes
+$views = $conf->process_views( 'config' => $views );
+my $dashboards = $conf->get_dashboards( 'config' => $views );
   
-}
 
-if (! $session->param('views')){
-  $views = $conf->read_dir( dir	=> $cfg_path . "/views" );
-  # replaces possible arrays in views with hashes
-  $views = $conf->process_views( 'config' => $views );
-  $dashboards = $conf->get_dashboards( 'config' => $views );
-  
-  $session->param('views', $views);
-  $session->param('dashboards', $dashboards);
-}else{
-  $views = $session->param('views');
-  $dashboards = $session->param('dashboards');
-}
+# loop for FastCGI
+while ( my $q = new CGI::Fast ){
 
-
-# process URL
-if (defined param){
+  # process URL
+  if (defined param){
 	
-  # JSON Header
-  print "Content-type: application/json charset=iso-8859-1\n\n";
-  my $json = undef;
+    # JSON Header
+    print "Content-type: application/json charset=iso-8859-1\n\n";
+    my $json = undef;
   
-  if (defined param("dashboard")){
+    if (defined param("dashboard")){
   	
-    # get dashboard data
-    my $dashboard = BPView::Data->new(
-    	 views		=> $views->{ param("dashboard") }{ 'views' },
-    	 provider	=> $config->{ 'provider' }{ 'source' },
-    	 provdata	=> $config->{ $config->{ 'provider' }{ 'source' } },
-       );	
-    $json = $dashboard->get_status();
+      # get dashboard data
+      my $dashboard = BPView::Data->new(
+    	   views	=> $views->{ param("dashboard") }{ 'views' },
+    	   provider	=> $config->{ 'provider' }{ 'source' },
+    	   provdata	=> $config->{ $config->{ 'provider' }{ 'source' } },
+         );	
+      $json = $dashboard->get_status();
        
-  }elsif (defined param("details")){
+    }elsif (defined param("details")){
   	
-  	# get details for this business process
-  	my $details = BPView::Data->new(
-  		provider	=> 'bpaddon',
-  		provdata	=> $config->{ 'bpaddon' },
-  		bp			=> param("details"),
-  	   );
-  	$json = $details->get_details();
+  	  # get details for this business process
+  	  my $details = BPView::Data->new(
+  		  provider	=> 'bpaddon',
+  		  provdata	=> $config->{ 'bpaddon' },
+  		  bp		=> param("details"),
+  	     );
+  	  $json = $details->get_details();
   	
+    }else{
+  	
+  	  print "Unknown paramater!\n";
+  	
+    }
+  
+    print $json;
+  
   }else{
-  	
-  	print "Unknown paramater!\n";
-  	exit 1;
-  	
+
+    print "Content-type: text/html\n\n";
+
+    # display web page
+    my $page = BPView::Web->new(
+ 	  src_dir		=> $config->{ 'bpview' }{ 'src_dir' },
+ 	  data_dir	=> $config->{ 'bpview' }{ 'data_dir' },
+ 	  site_url	=> $config->{ 'bpview' }{ 'site_url' },
+ 	  template	=> $config->{ 'bpview' }{ 'template' },
+    );
+    #   $page->login();
+    $page->display_page(
+       page		=> "main",
+       content	=> $dashboards,
+       refresh	=> $config->{ 'refresh' }{ 'interval' },
+    );
+
   }
-  
-  print $json;
-  exit 0;
-  
+
 }
-
-#print "Content-type: text/html\n\n";
-
-# display web page
-my $page = BPView::Web->new(
- 	src_dir		=> $config->{ 'bpview' }{ 'src_dir' },
- 	data_dir	=> $config->{ 'bpview' }{ 'data_dir' },
- 	site_url	=> $config->{ 'bpview' }{ 'site_url' },
- 	template	=> $config->{ 'bpview' }{ 'template' },
-);
-#   $page->login();
-   $page->display_page(
-     page		=> "main",
-     content	=> $dashboards,
-     refresh	=> $config->{ 'refresh' }{ 'interval' },
-);
-
 
 exit 0;
