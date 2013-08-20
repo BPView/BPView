6d8a759e51c3ae06b7b3a288ed2c297afcd80a51
Rene Koch <r.koch@ovido.at>
2013-08-20 17:57:25 +0200

    include bin/Makefile

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,7 @@ autoconf
 automake --add-missing
 
 # rename files
+mv bin/bp-addon_config_writer.pl cgi/bp-addon_config_writer.pl.in
 mv cgi/bpview.pl cgi/bpview.pl.in
 mv etc/bpview.yml etc/bpview.yml.in
 mv etc/datasource.yml etc/datasource.yml.in
