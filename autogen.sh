be85b3df399624694d56f13c779308650d0cde2f
Rene Koch <r.koch@ovido.at>
2013-08-20 18:05:17 +0200

    fixed typos

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ autoconf
 automake --add-missing
 
 # rename files
-mv bin/bp-addon_config_writer.pl cgi/bp-addon_config_writer.pl.in
+mv bin/bp-addon_config_writer.pl bin/bp-addon_config_writer.pl.in
 mv cgi/bpview.pl cgi/bpview.pl.in
 mv etc/bpview.yml etc/bpview.yml.in
 mv etc/datasource.yml etc/datasource.yml.in
