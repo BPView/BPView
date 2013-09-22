76acb47c77184a0ad3be871da3c5e5029fd7fe19
Peter Stöckl <p.stoeckl@ovido.at>
2013-09-22 06:58:36 +0200

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -11,6 +11,7 @@ mv bin/check_bp_status bin/check_bp_status.in
 mv cgi/bpview.pl cgi/bpview.pl.in
 mv etc/bpview.yml etc/bpview.yml.in
 mv etc/bp-addon.cfg etc/bp-addon.cfg.in
+mv etc/bpview.conf etc/bpview.conf.in
 mv etc/datasource.yml etc/datasource.yml.in
 mv sample-config/httpd.conf sample-config/httpd.conf.in
 mv sample-config/icinga/objects/bpview_templates.cfg sample-config/icinga/objects/bpview_templates.cfg.in 
