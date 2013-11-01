6cd19e03bce96c8932cd9f41ea3b5ec331b7d08d
Peter Stöckl <p.stoeckl@ovido.at>
2013-11-01 06:37:31 +0100

    bpview-fetcher added

--- autogen.sh
+++ autogen.sh
@@ -8,6 +8,7 @@ automake --add-missing
 # rename files
 mv bin/bpview_cfg_writer.pl bin/bpview_cfg_writer.pl.in
 mv bin/check_bp_status.pl bin/check_bp_status.pl.in
+mv bin/bpview-fetcher bin/bpview-fetcher
 mv cgi/bpview.pl cgi/bpview.pl.in
 mv etc/bpview.yml etc/bpview.yml.in
 mv etc/datasource.yml etc/datasource.yml.in
