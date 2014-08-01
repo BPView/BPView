8d12d840e2a56d8ef6510225f63505ce57db7d18
Max Oberberger <max@oberbergers.de>
2014-08-01 17:44:05 +0200

    remove check_bp_status relevant things (#34)

--- autogen.sh
+++ autogen.sh
@@ -8,7 +8,6 @@ automake --add-missing
 # rename files
 mv bin/bpview_cfg_writer.pl bin/bpview_cfg_writer.pl.in
 mv bin/bpviewd.pl bin/bpviewd.pl.in
-mv bin/check_bp_status.pl bin/check_bp_status.pl.in
 mv bin/bpview_reload.pl bin/bpview_reload.pl.in
 mv cgi/bpview.pl cgi/bpview.pl.in
 mv etc/bpview.yml etc/bpview.yml.in
