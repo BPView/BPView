5d00387bebe3392515152ef64dd07426214d9e2a
Rene Koch <rkoch@rk-it.at>
2014-10-02 12:00:47 +0200

    include bpviewd into package

--- autogen.sh
+++ autogen.sh
@@ -10,6 +10,7 @@ mv bin/bpviewd.pl bin/bpviewd.pl.in
 mv bin/bpview_reload.pl bin/bpview_reload.pl.in
 mv cgi/bpview.pl cgi/bpview.pl.in
 mv etc/bpview.yml etc/bpview.yml.in
+mv etc/bpviewd.yml etc/bpviewd.yml.in
 mv etc/datasource.yml etc/datasource.yml.in
 mv sample-config/httpd.conf sample-config/httpd.conf.in
 mv bpview.rh6.init bpview.rh6.init.in
